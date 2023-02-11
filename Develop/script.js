2dd14f39a1eae3e8b9c7994b8e063232f601bc15
Emily Miles <emilymiles193@gmail.com>
2023-02-10 19:47:38 -0500

    password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment code here
 var lengthOfPassword;
-
+var addLowerCase;
+var addUpperCase;
+var upperCase=["A","B","C","D","E","F","G","H","I",
+"J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+"q","r","s","t","u","v","w","x","y","z"];
+var num=["0","1","2","3","4","5","6","7","8","9"];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -16,12 +22,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
-lengthOfPassword = prompt("How long do you wish the password to be? Must be bettween 8 and 128 characters.")
-if(lengthofPassword<8){
-lengthOfPassword=prompt("Error. Password needs to be 8 or more characters.");
+
+function generatePassword() {
+  var lengthOfPassword = (prompt("How many characters would you like your password to contain?"));
+
+
+  while(lengthOfPassword < 8 || lengthOfPassword > 128) {
+      alert("Error. Password needs to be between 8 and 128 characters");
+      var lengthOfPassword = (prompt("How many characters would you like your password to contain?"));
+    } 
+
+    // Repeat back how many charactes the user will have  
+    alert("The number of characters your password will have is "+ lengthOfPassword.toString());
+      addUpper= confirm("Will this password be containing upper case letters?");
 }
-if(lengthOfPassword>128){
-  lengthOfPassword=prompt("Error. Password needs to be less than 128 characters.");
-}
-}
+
